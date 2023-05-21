bafde4db656a0ef2b8e97ed7bddaf7b408555ebc
k <1411193504@qq.com>
2023-05-21 15:28:54 +0800

    add 05 06

---
+++ 06_beep/beep.c
@@ -0,0 +1,41 @@
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/ide.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/gpio.h> 
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_gpio.h>
+
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+
+
+
+
+
+
+
+module_init(led_init);
+module_exit(led_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("k");
+
+
+
+
+
+
+
+
+
+
+
+
+
